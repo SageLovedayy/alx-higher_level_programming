902042109e8890d1b1a03a363ed9770bb93eb603
Loveday Nnabuife <116376154+SageLovedayy@users.noreply.github.com>
2023-11-27 23:38:48 +0100

    Add linked list cycle detection

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+
+/**
+* check_cycle - checks if linked list has cycle
+* @list: linked list
+* Return: 1 if cycle found, 0 if no cycle found
+*/
+int check_cycle(listint_t *list)
+{
+	listint_t *slow, *fast;
+
+	if (list == NULL || list->next == NULL)
+	{
+		return (0);
+	}
+
+	slow = malloc(sizeof(listint_t));
+	fast = malloc(sizeof(listint_t));
+
+	if (slow == NULL || fast == NULL)
+	{
+
+		return (0);
+	}
+
+	slow = list;
+	fast = list->next;
+
+	while (fast != NULL && fast->next != NULL)
+	{
+		if (fast == slow)
+		{
+
+			return (1); /*cycle found*/
+		}
+
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+
+
+
+	return (0);
+}
