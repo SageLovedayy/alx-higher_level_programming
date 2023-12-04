dc04f6c3284f7fc137b0aa25aebde2ede18a322d
Loveday Nnabuife <116376154+SageLovedayy@users.noreply.github.com>
2023-12-04 22:35:03 +0100

    Mod 10:34

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 
-def print_matrix_integer(matrix=[[]]):
-    for i in matrix:
-        print("{}".format(i))
+def print_matrix_integer(matrix=None):
+    if matrix is None:
+        return
+
+    for row in matrix:
+        for col_index, element in enumerate(row):
+            print("{:d}".format(element), end="")
+            if col_index != len(row) - 1:
+                print(" ", end="")
+        print("")
