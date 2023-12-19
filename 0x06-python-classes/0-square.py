a45ef5af405532e038a02d4d4343440cb52f2ecf
SageLovedayy <116376154+SageLovedayy@users.noreply.github.com>
2023-12-19 19:47:25 +0100

    Initial commit

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+"""My first square - defines a square"""
+
+class Square:
+    """empty square class"""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+"""1-square.py - This defines a square class"""
+class Square:
+    """class with private size attribute"""
+    def __init__(self, size):
+        self.__size = size
