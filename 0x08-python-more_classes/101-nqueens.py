0b49e1b9141c880e35fe37d66b95291454d8a593
SageLovedayy <116376154+SageLovedayy@users.noreply.github.com>
2024-01-05 00:17:22 +0100

    Initial commit

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """Solves the N-queens puzzle.
 
-Determines all possible solutions to placing N
-N non-attacking queens on an NxN chessboard.
+Determines all possible solutions to placing N non-attacking
+queens on an NxN chessboard.
 
 Example:
     $ ./101-nqueens.py N
