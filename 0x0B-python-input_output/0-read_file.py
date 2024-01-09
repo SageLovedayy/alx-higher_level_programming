c3ec8aee71738dfaa6141c9527e3b377f27113ea
SageLovedayy <116376154+SageLovedayy@users.noreply.github.com>
2024-01-10 00:23:27 +0100

    Initial commit

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""Read file module"""
+
+
+def read_file(filename=""):
+    """reads and prints a text file to stdout"""
+    with open(filename, "r", encoding="utf-8") as file:
+        content = file.read()
+        print(content)
