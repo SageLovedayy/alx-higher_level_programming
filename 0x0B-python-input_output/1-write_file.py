2d8e87272d3f1ce8d87cf3ba0b5dbf007c0ca17b
SageLovedayy <116376154+SageLovedayy@users.noreply.github.com>
2024-01-10 01:29:26 +0100

    Mod 1:28

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-
-"""Write to file Module"""
+"""
+1-write_file module
+writes to text file and returns num chars written
+"""
 
 
 def write_file(filename="", text=""):
-    """
-    writes a string to a text file
-    Return: number of characters written
-    """
-    with open(filename, "w", encoding="utf-8") as file:
-        return (file.write(text))
+    """writes to text file and returns num chars written"""
+    with open(filename, mode="w", encoding="utf-8") as f:
+        return (f.write(text))
