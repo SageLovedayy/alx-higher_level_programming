4896b558ef800e12f19b71febd3acaabaf188fe1
SageLovedayy <116376154+SageLovedayy@users.noreply.github.com>
2024-03-17 22:22:54 +0100

    Initial commit

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Lists all states from the database
+Takes as arguments: mysql username, mysql password, database name
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+
+    # database connection
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+
+    # Execute queries with SQL
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    for row in cursor.fetchall():
+        print(row)
+    cursor.close()
+    db.close()
