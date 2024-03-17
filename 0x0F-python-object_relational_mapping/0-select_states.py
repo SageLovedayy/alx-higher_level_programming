2aa28052b182561d0d8908664d6c619e2481e4ce
SageLovedayy <116376154+SageLovedayy@users.noreply.github.com>
2024-03-17 22:29:40 +0100

    Objects, scopes, closures

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
 
     # Execute queries with SQL
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    cursor.execute("SELECT * FROM states")
     for row in cursor.fetchall():
         print(row)
     cursor.close()
